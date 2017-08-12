c4c3e4c6dc115c2311dc1fd43e4c417fb71462d2
Gagan Deep Singh <me@gagan.pro>
2017-08-12 12:10:19 +0530

    Basic structure of the plugin

--- admin/class-easy-primary-category-admin.php
+++ admin/class-easy-primary-category-admin.php
@@ -8,6 +8,9 @@ if ( !class_exists( 'Easy_Primary_Category_Admin' ) ) {
 
 	class Easy_Primary_Category_Admin {
 
+		/**
+		 * @var Easy_Primary_Category_Admin The instance of the class Easy_Primary_Category_Admin
+		 */
 		protected static $instance = null;
 
 		function __construct() {
@@ -15,6 +18,10 @@ if ( !class_exists( 'Easy_Primary_Category_Admin' ) ) {
 		}
 
 		/**
+		 * Returns the current instance of the class
+		 * 
+		 * @since 0.1
+		 * 
 		 * @return Easy_Primary_Category_Admin Returns the current instance of the class
 		 */
 		public static function get_instance() {
@@ -25,6 +32,88 @@ if ( !class_exists( 'Easy_Primary_Category_Admin' ) ) {
 			}
 
 			return self::$instance;
+		}
+
+		/**
+		 * Registers the actions and filters for the Admin UI
+		 * 
+		 * @since 0.1
+		 * 
+		 * @return void
+		 */
+		public function register_hooks() {
+			
+		}
+
+		/**
+		 * Enqueues all the assets needed for the primary term interface
+		 *
+		 * @return void
+		 */
+		public function enqueue_scripts() {
+			
+		}
+
+		/**
+		 * Save the primary term for a specific taxonomy
+		 *
+		 * @param int     $post_id  Post ID to save primary term for.
+		 * @param WP_Term $taxonomy Taxonomy to save primary term for.
+		 * 
+		 * @return void
+		 */
+		protected function save_primary_term( $post_id, $taxonomy ) {
+			
+		}
+
+		/**
+		 * Saves all selected primary terms
+		 * 
+		 * @since 0.1
+		 *
+		 * @param int $post_id Post ID to save primary terms for.
+		 */
+		public function save_primary_terms( $post_id ) {
+			
+		}
+
+		/**
+		 * /**
+		 * Get the id of the primary term
+		 * 
+		 * @since 0.1
+		 *
+		 * @param string $taxonomy_name Taxonomy name for the term.
+		 *
+		 * @return int primary term id
+		 */
+		protected function get_primary_term( $taxonomy_name ) {
+			
+		}
+
+		/**
+		 * Returns all the taxonomies for which the primary term selection is enabled
+		 * 
+		 * @since 0.1
+		 *
+		 * @param int $post_id Default current post ID.
+		 * @return array
+		 */
+		protected function get_primary_term_taxonomies( $post_id = null ) {
+			
+		}
+
+		/**
+		 * Generate the primary term taxonomies.
+		 * 
+		 * @since 0.1
+		 *
+		 * @param int $post_id ID of the post.
+		 *
+		 * @return array
+		 */
+		protected function generate_primary_term_taxonomies( $post_id ) {
+			
 		}
 
 	}
