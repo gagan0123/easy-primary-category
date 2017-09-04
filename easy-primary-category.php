129c98cd765f4169eeffc45fc4a50d192757c820
Gagan Deep Singh <me@gagan.pro>
2017-09-04 09:09:17 +0530

    Renamed EPC_VERSION to EPC_ASSETS_VERSION. Done to make sure that cache of static assets is flushed in case we make changes to either JS or CSS files.

--- easy-primary-category.php
+++ easy-primary-category.php
@@ -3,7 +3,7 @@
  * Plugin Name: Easy Primary Category
  * Plugin URI:  https://gagan0123.com/
  * Description: Allows you to choose primary category for posts and custom post types
- * Version:     1.0
+ * Version:     1.1
  * Author:      Gagan Deep Singh
  * Author URI:  https://gagan0123.com
  * License:     GPLv2
@@ -20,15 +20,32 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'EPC_PATH' ) ) {
+	/**
+	 * Path to the plugin folder.
+	 *
+	 * @since 0.1
+	 */
 	define( 'EPC_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
 }
 
 if ( ! defined( 'EPC_URL' ) ) {
+	/**
+	 * URL to the plugin folder.
+	 *
+	 * @since 0.1
+	 */
 	define( 'EPC_URL', trailingslashit( plugins_url( '', __FILE__ ) ) );
 }
 
-if ( ! defined( 'EPC_VERSION' ) ) {
-	define( 'EPC_VERSION', '1.0' );
+if ( ! defined( 'EPC_ASSETS_VERSION' ) ) {
+	/**
+	 * Version number for plugin assets.
+	 *
+	 * Need to change only when there is some change in JS or CSS file.
+	 *
+	 * @since 1.1
+	 */
+	define( 'EPC_ASSETS_VERSION', '1.1' );
 }
 
 /**
